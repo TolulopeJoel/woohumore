b8bdd7fb7f3fb5d0dbb0134a1bf715618516e780
Tolu Joel <toluisjoel@gmail.com>
2023-05-22 01:33:32 +0100

    Added post and news source serializers

--- news/serializers.py
+++ news/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Post
+from .models import NewsSource, Post
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -13,9 +13,46 @@ class PublicPostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = [
+            'id',
             'title',
             'content',
             'slug',
             'image',
             'updated_at',
         ]
+
+
+class NewsSourceSerializer(serializers.ModelSerializer):
+    posts = PublicPostSerializer(many=True)
+    total_posts_count = serializers.SerializerMethodField()
+    published_posts_count = serializers.SerializerMethodField()
+    unpublished_posts_count = serializers.SerializerMethodField()
+
+    class Meta:
+        model = NewsSource
+        fields = [
+            'id',
+            'site_name',
+            'site_url',
+            'total_posts_count',
+            'published_posts_count',
+            'unpublished_posts_count',
+            'posts',
+        ]
+
+    def get_total_posts_count(self, obj):
+        posts = obj.posts.all()
+
+        return posts.count()
+
+    def get_published_posts_count(self, obj):
+        posts = obj.posts.all()
+        published_posts = posts.filter(published=True)
+
+        return published_posts.count()
+
+    def get_unpublished_posts_count(self, obj):
+        posts = obj.posts.all()
+        unpublished_posts = posts.filter(published=False)
+
+        return unpublished_posts.count()
