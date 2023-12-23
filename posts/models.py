8a19087742aa6dc00c222639d7288acd08663067
Tolu Joel <toluisjoel@gmail.com>
2023-12-23 03:13:06 +0100

    Add new field to post model

--- posts/models.py
+++ posts/models.py
@@ -24,6 +24,7 @@ class Post(models.Model):
     )
     title = models.CharField(max_length=255)
     body = models.TextField()
+    link_to_news = models.URLField()
     image = models.URLField(blank=True)
     published = models.BooleanField(default=False)
     published_date = models.DateTimeField(blank=True, null=True)
