21b3bd61d72baeb8455e102b3adf55858c3a19a5
Tolu Joel <toluisjoel@gmail.com>
2024-01-04 12:27:39 +0100

    Add tests for scrpaer

--- scraper/tests.py
+++ scraper/tests.py
@@ -1,3 +1,48 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient, APITestCase
+from posts.models import Post, Source
 
-# Create your tests here.
+class ScrapePostTests(APITestCase):
+
+    def setUp(self):
+        self.source = Source.objects.create(
+            name='Example News',
+            news_page='https://example.com/news',
+            link_tag='a',
+            link_tag_class='post-link',
+            title_tag='h2',
+            domain='https://example.com'
+        )
+
+    def test_scrape_post_list_view(self):
+        """
+        Test the scrape post list view.
+        """
+        url = reverse('scrape-post-list')
+        response = self.client.get(url)
+        print(response.content)
+        self.assertScrapedPost(response)
+
+        # Add more assertions based on your specific response structure
+
+    def test_scrape_post_detail_view(self):
+        """
+        Test the scrape post detail view.
+        """
+        post = Post.objects.create(
+            news_source=self.source,
+            title='Test Post',
+            body='This is a test post.',
+            link_to_news='https://example.com/test-post'
+        )
+
+        url = reverse('scrape-post-detail')
+        response = self.client.get(url)
+        self.assertScrapedPost(response)
+
+    def assertScrapedPost(self, response):
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('status', response.data)
+        self.assertEqual(response.data['status'], 'success')
+
--- scraper/urls.py
+++ scraper/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('fetch-posts/', views.ScrapePostListView.as_view()),
-    path('fetch-posts-detail/', views.ScrapePostDetailView.as_view())
+    path('fetch-posts/', views.ScrapePostListView.as_view(), name='scrape-post-list'),
+    path('fetch-posts-detail/', views.ScrapePostDetailView.as_view(), name='scrape-post-detail')
 ]
