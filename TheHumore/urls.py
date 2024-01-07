bc09fdecba8e08f0b59150827f1854c2c20f6b49
Tolu Joel <toluisjoel@gmail.com>
2024-01-07 22:42:30 +0100

    Update urlpatterns to include 'videos' URLs

--- TheHumore/urls.py
+++ TheHumore/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('posts.urls')),
-    path('scraper/', include('scraper.urls')),
+    path('', include('scraper.urls')),
+    path('', include('videos.urls')),
 ]
