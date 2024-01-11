02e233004f724eec1684f7c5f2e5eabc6449bc64
Tolu Joel <toluisjoel@gmail.com>
2024-01-11 13:45:22 +0100

    Add News model

--- news/models.py
+++ news/models.py
@@ -1,3 +1,29 @@
+from cloudinary.models import CloudinaryField
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+from posts.models import Post
+
+
+class News(models.Model):
+    title = models.CharField(max_length=255)
+    video = CloudinaryField()
+    posts = models.ManyToManyField(Post, related_name='news', blank=True)
+    published = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        ordering = ('-published_date',)
+
+    def save(self, *args, **kwargs):
+        if self.published and not self.published_date:
+            self.published_date = timezone.now()
+        elif not self.published and self.published_date:
+            self.published_date = None
+        return super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return self.title
