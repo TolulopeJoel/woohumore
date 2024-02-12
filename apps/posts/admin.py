353dc599c668a7f15ce42c6d47badfae7125188c
Tolu Joel <toluisjoel@gmail.com>
2024-02-12 15:25:30 +0100

    refactor: Modify list display for source admin page

--- apps/posts/admin.py
+++ apps/posts/admin.py
@@ -6,5 +6,10 @@ from .models import Post, Source
 class PostAdmin(admin.ModelAdmin):
     list_filter = ['news_source', 'has_audio', 'has_video', 'is_published']
 
+
+class SourceAdmin(admin.ModelAdmin):
+    list_display = ['name', 'active']
+
+
 admin.site.register(Post, PostAdmin)
-admin.site.register(Source)
+admin.site.register(Source, SourceAdmin)
