55cb8a38c95bbe7271756cb673ecfa0eb438690d
Tolu Joel <toluisjoel@gmail.com>
2024-03-05 16:35:57 +0100

    chore: Upate database settings

--- TheHumore/settings.py
+++ TheHumore/settings.py
@@ -101,12 +101,7 @@ WSGI_APPLICATION = 'TheHumore.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {'default': env.db('DATABASE_URL')}
 
 
 # Password validation
