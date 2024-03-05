ca680ab374fea3d423b3836dd9caa26ee8af6ea0
Tolu Joel <toluisjoel@gmail.com>
2024-03-05 15:26:06 +0100

    chore: Update allowed hosts settings

--- TheHumore/settings.py
+++ TheHumore/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 
 
 # Application definition
